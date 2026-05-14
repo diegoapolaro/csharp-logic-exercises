78125f0dd6b6bd3e1a7be56ecb8ce60d11ccf75e
Diego Apolaro <spkf1v3.twitch@gmail.com>
2026-05-14 00:34:20 -0300

    feat: add conditional initial exercises

---
+++ Exercicios/02-condicoes/ex00NúmeroPositivoOuNegativo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaCSharp.Exercicios._02_condicoes
+{
+    internal class ex00NúmeroPositivoOuNegativo
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Digite um número para verificar se é positivo ou negativo: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num == 0)
+            {
+                Console.WriteLine("0 não é positivo nem negativo");
+            }
+            else if(num > 0)
+            {
+                Console.WriteLine(num + " é um número positivo");
+            }
+            else
+            {
+                Console.WriteLine(num + " é um número negativo");
+            }
+
+        }
+    }
+}
---
+++ Exercicios/02-condicoes/ex01ParOuImpar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaCSharp.Exercicios._02_condicoes
+{
+    internal class ex01ParOuImpar
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Digite um número para verificar se é par ou impar: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " é um número par");
+            }
+            else
+            {
+                Console.WriteLine(num + " é um número impar");
+            }
+        }
+    }
+}
